db72a2db54006b4d285e6cd052a2a78a5c4de613
HaydenDippL <hdippel@wisc.edu>
2025-01-11 21:11:02 -0600

    Added media directory

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_URL =  "media"
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
