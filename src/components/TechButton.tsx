ee65291e0fd3460eff34a81a66453f411cd2a5e5
Hayden Dippel <hdippel@wisc.edu>
2025-02-22 23:29:09 -0600

    Buttons colored in on small screens

--- src/components/TechButton.tsx
+++ src/components/TechButton.tsx
@@ -1,7 +1,22 @@
 import { useNavigate } from "react-router";
 import { TechButtonProps, badge_colors, badge_text_colors } from "../types/TechTypes";
+import { useEffect, useState } from "react";
 
 export default function TechButton({ tech, link }: TechButtonProps) {
+    const BREAKPOINT_FILL_STATE: number = 640;
+    const [fill_state, set_fill_state] = useState<boolean>(window.innerWidth <= BREAKPOINT_FILL_STATE);
+
+    useEffect(() => {
+        const handle_resize = () => {
+            set_fill_state(window.innerWidth <= BREAKPOINT_FILL_STATE);
+        }
+
+        handle_resize();
+        window.addEventListener("resize", handle_resize);
+
+        return () => window.removeEventListener("resize", handle_resize);
+    }, []);
+
     const badge_color: string = badge_colors[tech];
     const text_color: string = badge_text_colors[tech];
     
@@ -24,23 +39,37 @@ export default function TechButton({ tech, link }: TechButtonProps) {
         if (link) navigate(link);
     }
 
-    return <div className="inline-block">
-        <button
-            className={`
-                transform transition-all duration-300 ease-in-out hover:scale-125 focus:scale-125
-                content-text px-2.5 py-0.5 border-2 rounded-lg
-                bg-transparent
-                border-solid border-white/50 text-white
-                relative hover:z-[50] focus:z-[50]
-            `}
-            // style={transparent_styles}
-            onMouseEnter={(e) => set_colors(e)}
-            onMouseLeave={(e) => reset_colors(e)}
-            onFocus={(e) => set_colors(e)}
-            onBlur={(e) => reset_colors(e)}
-            onClick={() => navigate_to_link()}
-        >
-            {tech}
-        </button>
-    </div>
+    if (fill_state) {
+        return <div className="inline-block">
+            <button
+                className="content-text px-2.5 py-0.5 border-2 rounded-lg border-solid"
+                style={{
+                    backgroundColor: badge_color,
+                    borderColor: badge_color,
+                    color: text_color
+                }}
+            >
+                { tech }
+            </button>
+        </div>
+    } else {
+        return <div className="inline-block">
+            <button
+                className="
+                    transform transition-all duration-300 ease-in-out hover:scale-125 focus:scale-125
+                    content-text px-2.5 py-0.5 border-2 rounded-lg
+                    bg-transparent
+                    border-solid border-white/50 text-white
+                    relative hover:z-[50] focus:z-[50]
+                "
+                onMouseEnter={(e) => set_colors(e)}
+                onMouseLeave={(e) => reset_colors(e)}
+                onFocus={(e) => set_colors(e)}
+                onBlur={(e) => reset_colors(e)}
+                onClick={() => navigate_to_link()}
+            >
+                {tech}
+            </button>
+        </div>
+    }
 }
